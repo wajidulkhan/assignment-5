f2fcec75eff699b1bd695a368658806da16d9464
wajidulkhan <wajidulkhan2001@gmail.com>
2025-09-07 20:00:47 +0600

    fix all error

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let loveCount = 0;
-let coin = 10;
+let coin = 20;
 const callValue = [];
-let copy = 10;
+let copy = 0;
 const notificationVales = document.getElementById("notification-id");
 
 function loveReact() {
@@ -53,56 +53,11 @@ function notification(values) {
   }
 }
 
-// copy Function
-function copyFunction(id) {
-  if (copy > 0) {
-    const copyText = document.getElementById(id);
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.innerText);
-    // Alert the copied text
-    alert("Copied the text: " + copyText.innerText);
-    copy--;
-    document.getElementById("copy-number").innerText = copy;
-  } else {
-    alert("copy are available now");
-  }
-}
-
-document.getElementById("copy-number").innerText = copy;
-
-// Heart
-document.getElementById("emergency-heart").addEventListener("click", loveReact);
-document.getElementById("police-heart").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-document.getElementById("fire-service").addEventListener("click", loveReact);
-
-// call
-document
-  .getElementById("emergency-call")
-  .addEventListener("click", function (e) {
-    const callTitle = document.getElementById("National").innerText;
-    console.log(callTitle);
-    const callNumber = document.getElementById("call-number").innerText;
-    const a = callService(callTitle, callNumber);
-
-    const callData = {
-      serviceName: callTitle,
-      serviceNumber: callNumber,
-      Dates: new Date().toLocaleTimeString(),
-    };
-    callValue.push(callData);
-    notification(callValue);
-  });
-
-document.getElementById("police-call").addEventListener("click", function (e) {
-  const callTitle = document.getElementById("Police-Helpline").innerText;
-  const callNumber = document.getElementById("Police-number").innerText;
-  callService(callTitle, callNumber);
+function callBtn(value1, value2) {
+  const callTitle = document.getElementById(value1).innerText;
+  console.log(callTitle);
+  const callNumber = document.getElementById(value2).innerText;
+  const a = callService(callTitle, callNumber);
 
   const callData = {
     serviceName: callTitle,
@@ -111,13 +66,130 @@ document.getElementById("police-call").addEventListener("click", function (e) {
   };
   callValue.push(callData);
   notification(callValue);
+}
+
+// copy Function
+function copyFunction(id) {
+  const copyText = document.getElementById(id);
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.innerText);
+  // Alert the copied text
+  alert("Copied the text: " + copyText.innerText);
+  copy++;
+  document.getElementById("copy-number").innerText = copy;
+}
+
+document.getElementById("copy-number").innerText = copy;
+
+// Heart
+document.getElementById("emergency-heart").addEventListener("click", loveReact);
+document.getElementById("police-heart").addEventListener("click", loveReact);
+document.getElementById("fire-service").addEventListener("click", loveReact);
+document.getElementById("Ambulance").addEventListener("click", loveReact);
+document.getElementById("Anti-Corruption").addEventListener("click", loveReact);
+document.getElementById("Women").addEventListener("click", loveReact);
+document
+  .getElementById("Electricity-Helpline")
+  .addEventListener("click", loveReact);
+document.getElementById("Brac").addEventListener("click", loveReact);
+document
+  .getElementById("Bangladesh-Railway")
+  .addEventListener("click", loveReact);
+
+// call
+document
+  .getElementById("emergency-call")
+  .addEventListener("click", function (e) {
+    callBtn("National", "call-number");
+  });
+
+document.getElementById("police-call").addEventListener("click", function (e) {
+  callBtn("Police-Helpline", "Police-number");
 });
 
+document
+  .getElementById("fire-service-btn")
+  .addEventListener("click", function (e) {
+    callBtn("fire-service-title", "fire-service-number");
+  });
+
+document
+  .getElementById("Ambulance-btn")
+  .addEventListener("click", function (e) {
+    callBtn("Ambulance-title", "Ambulance-number");
+  });
+
+document
+  .getElementById("Anti-Corruption-btn")
+  .addEventListener("click", function (e) {
+    callBtn("Anti-Corruption-title", "Anti-Corruption-number");
+  });
+
+document.getElementById("Women-btn").addEventListener("click", function (e) {
+  callBtn("Women-title", "Women-number");
+});
+
+document
+  .getElementById("Electricity-btn")
+  .addEventListener("click", function (e) {
+    callBtn("Electricity-title", "Electricity-number");
+  });
+
+document.getElementById("Brac-btn").addEventListener("click", function (e) {
+  callBtn("Brac-title", "Brac-number");
+});
+
+document
+  .getElementById("Bangladesh-Railway-btn")
+  .addEventListener("click", function (e) {
+    callBtn("Bangladesh-Railway-title", "Bangladesh-Railway-number");
+  });
 // clear - Btn;
 document.getElementById("clear-btn").addEventListener("click", function () {
+  callValue.length = 0;
   notificationVales.innerHTML = "";
 });
 
+// Copy
 document.getElementById("copy-national").addEventListener("click", function () {
   copyFunction("call-number");
 });
+
+document.getElementById("copy-police").addEventListener("click", function () {
+  copyFunction("Police-number");
+});
+
+document.getElementById("copy-Fire").addEventListener("click", function () {
+  copyFunction("fire-service-number");
+});
+
+document
+  .getElementById("copy-Ambulance")
+  .addEventListener("click", function () {
+    copyFunction("Ambulance-number");
+  });
+
+document
+  .getElementById("copy-Anti-Corruption")
+  .addEventListener("click", function () {
+    copyFunction("Anti-Corruption-number");
+  });
+
+document.getElementById("copy-Women").addEventListener("click", function () {
+  copyFunction("Women-number");
+});
+
+document.getElementById("copy-Brac").addEventListener("click", function () {
+  copyFunction("Brac-number");
+});
+document
+  .getElementById("copy-Electricity")
+  .addEventListener("click", function () {
+    copyFunction("Electricity-number");
+  });
+
+document
+  .getElementById("copy-Bangladesh-Railway")
+  .addEventListener("click", function () {
+    copyFunction("Bangladesh-Railway-number");
+  });
