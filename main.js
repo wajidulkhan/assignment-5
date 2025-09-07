edd6d5a2324576c7793b4a2f9dd225d93ce07ba9
wajidulkhan <wajidulkhan2001@gmail.com>
2025-09-07 14:27:08 +0600

    notification Section are Ready

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let loveCount = 0;
 let coin = 10;
+const callValue = [];
 
 function loveReact() {
   const Count = document.getElementById("love-react");
@@ -21,6 +22,30 @@ function callService(call, callNumber) {
   }
 }
 
+function notification(values) {
+  const notificationVales = document.getElementById("notification-id");
+  notificationVales.innerHTML = "";
+  for (items of values) {
+    const div = document.createElement("div");
+    console.log(items.serviceName);
+    div.innerHTML = `
+
+     <div
+              class="flex justify-between items-center bg-gray-100 rounded-lg p-3"
+            >
+    <div>
+                <h3 class="text-sm font-semibold">${items.serviceName}</h3>
+                <span class="text-gray-800">${items.serviceNumber}</span>
+              </div>
+              <span class="text-sm text-gray-800">${items.Dates}</span>
+            </div>
+    </div>
+    `;
+
+    notificationVales.appendChild(div);
+  }
+}
+
 // Heart
 document.getElementById("emergency-heart").addEventListener("click", loveReact);
 document.getElementById("police-heart").addEventListener("click", loveReact);
@@ -31,12 +56,29 @@ document
   .getElementById("emergency-call")
   .addEventListener("click", function (e) {
     const callTitle = document.getElementById("National").innerText;
+    console.log(callTitle);
     const callNumber = document.getElementById("call-number").innerText;
-    callService(callTitle, callNumber);
+    const a = callService(callTitle, callNumber);
+
+    const callData = {
+      serviceName: callTitle,
+      serviceNumber: callNumber,
+      Dates: new Date().toLocaleTimeString(),
+    };
+    callValue.push(callData);
+    notification(callValue);
   });
 
 document.getElementById("police-call").addEventListener("click", function (e) {
   const callTitle = document.getElementById("Police-Helpline").innerText;
   const callNumber = document.getElementById("Police-number").innerText;
   callService(callTitle, callNumber);
+
+  const callData = {
+    serviceName: callTitle,
+    serviceNumber: callNumber,
+    Dates: new Date().toLocaleTimeString(),
+  };
+  callValue.push(callData);
+  notification(callValue);
 });
